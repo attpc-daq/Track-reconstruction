b6ccdcccf76c976b7888dd0a14b7205cac17c8a8
HuangkaiWu <17858287896@163.com>
2022-07-29 14:41:01 +0800

    root LinkDef

--- src/FitterLinkDef.hh
+++ src/FitterLinkDef.hh
@@ -5,15 +5,5 @@
 #pragma link off all functions;
 
 #pragma link C++ class HoughTransform2D;
-#pragma link C++ class ActarSimSimpleTrack;
-// #pragma link C++ class ActarSimPrimaryInfo;
-// #pragma link C++ class ActarSimBeamInfo;
-#pragma link C++ class ActarSimSilHit;
-#pragma link C++ class ActarPadSignal;
-#pragma link C++ class projectionOnPadPlane;
-#pragma link C++ class padsGeometry;
-#pragma link C++ class amplificationManager;
-#pragma link C++ class driftManager;
-// #pragma link C++ class MTrack;
 
 #endif
